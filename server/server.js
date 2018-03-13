4fefd580812d5adbf49733919a10abbbf0556e91
Sorin Chereji <schereji@Sorin.local>
2018-03-13 16:53:35 -0700

    Push to heroku

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-mongoose.connect('mongodb://admin:admin@ds257848.mlab.com:57848/voice_memo');
+mongoose.connect('mongodb://heroku_jp4tjvlj:ugfod4op0ur32gnq331l6lh12u@ds113749.mlab.com:13749/heroku_jp4tjvlj');
 app.use('/', router);
 
 module.exports=app;
